99182422f843e667f34a8f36f7e192b204555f62
Mehmet Onurcan KAYA <monurcan55@gmail.com>
2014-11-22 23:21:37 +0200

    sad

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -127,8 +127,11 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   var message = won ? "Helal!" : "Oyun Bitti!";
-  if(won){
-    $("body").append('<div id="hll"><iframe width="0" height="0" src="http://www.youtube.com/embed/U1ei5rwO7ZI?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe><img src="http://grafikdesign.files.wordpress.com/2014/03/recep-tayyip-erdogan-006.png"><h3>HELAL!</h3></div>');
+  if(won == 1){
+    $("body").append('<div id="hll"><a class="restart-button ress">x</a><iframe width="0" height="0" src="http://www.youtube.com/embed/U1ei5rwO7ZI?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe><img src="http://grafikdesign.files.wordpress.com/2014/03/recep-tayyip-erdogan-006.png"><h3>HELAL!</h3></div>');
+    $(".ress").click(function(){
+      $("#hll").remove();
+    });
   }
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
